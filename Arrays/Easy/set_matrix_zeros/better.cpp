4f15b7e5c55ff28243575843fe7cb47e42f9fcfe
Kchanana2025 <kanikachanana2025@gmail.com>
2023-07-05 13:31:21 +0530

    array easy

---
+++ Arrays/Easy/set_matrix_zeros/better.cpp
@@ -0,0 +1,49 @@
+// time:O(n^2)
+// space:O(m+n)
+void set_it(vector<vector<int>> &matrix, int *arr1, int *arr2)
+{
+    int m = matrix.size();
+    int n = matrix[0].size();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+
+            if (arr1[i] || arr2[j]) // arr1 aur arr2 ka size bhjne ki zarurat nai hai ye condition chalegi hi un elements ke lie jo matrix mein honge
+            {
+
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
+class Solution
+{
+public:
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+        int m = matrix.size();    // NO OF ROWS
+        int n = matrix[0].size(); // NO OF COLUMNS
+        int arr1[1000] = {0};     // rows
+        int arr2[1000] = {0};     // columns
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    arr1[i] = 1;
+                    arr2[j] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++)
+            cout << arr1[i] << " ";
+        cout << endl;
+        for (int i = 0; i < n; i++)
+            cout << arr2[i] << " ";
+        cout << endl;
+        set_it(matrix, arr1, arr2);
+    }
+};
--- Arrays/Easy/set_matrix_zeros/set_matrix_zeros_brute_force.cpp
+++ Arrays/Easy/set_matrix_zeros/set_matrix_zeros_brute_force.cpp
@@ -1,29 +1,38 @@
+// time=(m*n)*(m+n)+(m*n)~O(n^3)
+// space:O(1)
+#include <iostream>
+#include <vector>
+using namespace std;
 void set_row(int i, vector<vector<int>> &matrix)
 {
-    int n = matrix[i].size();
-    for (int j = 0; j < n; j++) // traversing all columns and setting all columns of row ith to be -1
+    int n = matrix[0].size();
+    for (int j = 0; j < n; j++) // traversing all columns and setting all columns of row ith to be 21474836
     {
-        matrix[i][j] = -1; // we are setting it to -1 because if we set it to 0 then when we again traverse to find zeros we will find extra zeros and therefore we will set those rows and columns to be zero which shouldn't be
+        if (matrix[i][j] != 0) // I didnt write this
+            matrix[i][j] = 21474836;
     }
 }
 void set_column(int j, vector<vector<int>> &matrix)
 {
     int m = matrix.size();
-    for (int i = 0; i < m; i++) // traversing all the rwos and setting that particular column as -1
+    for (int i = 0; i < m; i++) // traversing all the rwos and setting that particular column as 21474836
     {
-        matrix[i][j] = -1;
+        if (matrix[i][j] != 0)
+            matrix[i][j] = 21474836;
     }
 }
 void set_it(vector<vector<int>> &matrix)
 {
     int m = matrix.size();
+    int n = matrix[0].size();
     for (int i = 0; i < m; i++)
     {
-        int n = matrix[i].size();
         for (int j = 0; j < n; j++)
         {
-            if (matrix[i][j] == -1)
+
+            if (matrix[i][j] == 21474836)
             {
+
                 matrix[i][j] = 0;
             }
         }
@@ -34,10 +43,10 @@ class Solution
 public:
     void setZeroes(vector<vector<int>> &matrix)
     {
-        int m = matrix.size(); // NO OF ROWS
+        int m = matrix.size();    // NO OF ROWS
+        int n = matrix[0].size(); // NO OF COLUMNS
         for (int i = 0; i < m; i++)
         {
-            int n = matrix[i].size(); // NO OF COLUMNS
             for (int j = 0; j < n; j++)
             {
                 if (matrix[i][j] == 0)
